76571027defc202269b9256fd2781b0f5b3b3b6c
makerio90 <makerio0503@gmail.com>
2021-05-08 01:52:28 -0400

    innit

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# myprj" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:makerio90/myprj.git
+git push -u origin main
