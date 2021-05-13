353c30efb193734953b198df3dd23a7ef07ae533
makerio90 <makerio0503@gmail.com>
2021-05-12 23:06:50 -0400

    marked

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-function go(){
-    var fileupload = document.getElementById('prj-input-1').files[0]
-    var zip = new JSZip();
-    zip.loadAsync(fileupload, {checkCRC32: true, createFolders: true}).then(function (zip) {
-        zip.file("INSTRUCTIONS.md").async("string").then(function(content) {
-            document.getElementById('_instructions').innerHTML = content
-        }, function(e) {
-            halfmoon.initStickyAlert({
-                content: e,
-                title: "error getting instructions",
-                alertType: "alert-danger"
-            });
-        });
-        
-    }, function (e) {
-        halfmoon.initStickyAlert({
-            content: e,
-            title: "error reading file",
-            alertType: "alert-danger"
-        });
-    });
+function go() {
+    var fileupload = document.getElementById('prj-input-1').files[0];
+    new JSZip()
+        .loadAsync(fileupload, {checkCRC32: true, createFolders: true})
+        .then(
+            function (zip) {
+                zip.file('INSTRUCTIONS.md')
+                    .async('string')
+                    .then(
+                        function (content) {
+                            document.getElementById('_instructions').innerHTML =
+                                content;
+                        },
+                        function (e) {
+                            halfmoon.initStickyAlert({
+                                content: e,
+                                title: 'error getting instructions',
+                                alertType: 'alert-danger',
+                            });
+                        }
+                    );
+            },
+            function (e) {
+                halfmoon.initStickyAlert({
+                    content: e,
+                    title: 'error reading file',
+                    alertType: 'alert-danger',
+                });
+            }
+        );
 }
